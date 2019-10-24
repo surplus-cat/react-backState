a553d371926943debe50b6371cfdd50cd0724258
Corn <1073406997@qq.com>
2019-10-24 09:50:20 +0800

    add: 完成utils插件目录和loading加载组件

---
+++ src/components/Loading/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.less';
+
+function Loading() {
+
+  return (
+    <div className="loading-wrapper">
+      <div className="loading">
+        <div className="loading-left"></div>
+        <div className="loading-right"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loading;
---
+++ src/utils/routerLoadable.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import loadable from 'react-loadable';
+import LoadingView from '../components/Loading';
+
+
+function asyncImport(Loader) {
+
+  function Loading(props) {
+    if (props.error) 
+      return <div>Error !</div>
+    else if (props.pastDelay) 
+      return <LoadingView />
+    else
+      return null;
+  }
+
+  return loadable({
+    Loader,
+    loading: Loading
+  })
+}
+
+export {
+  asyncImport
+}
